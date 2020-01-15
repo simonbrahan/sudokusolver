a9615aff7ba15b7acb0f04a3b53fb221b6c998ab
Simon Brahan <tntpiranha@gmail.com>
2020-01-15 15:38:30 +0000

    Simplified parser to only accept 9 x 9 game boards. Pretty print parsed board for testing

--- src/input_parser.rs
+++ src/input_parser.rs
@@ -1,39 +1,24 @@
-use std::collections::HashMap;
+pub fn parse(input: &str) -> Result<Vec<Option<usize>>, &str> {
+    let game_board: Vec<Option<usize>> = input
+        .chars()
+        .filter(|char| !char.is_whitespace())
+        .collect::<String>()
+        .split(',')
+        .map(|num| num.parse().ok())
+        .collect();
 
-pub fn parse(input: &str) -> Result<(Vec<Option<usize>>, usize), &str> {
-    let valid_board_lengths: HashMap<usize, usize> = [
-        (1, 1),
-        (4, 2),
-        (9, 3),
-        (16, 4),
-        (25, 5),
-        (36, 6),
-        (49, 7),
-        (64, 8),
-        (81, 9),
-    ]
-    .iter()
-    .cloned()
-    .collect();
-
-    let game_board: Vec<Option<usize>> = input.split(',').map(|num| num.parse().ok()).collect();
-
-    let maybe_max_symbol = valid_board_lengths.get(&game_board.len());
-
-    if maybe_max_symbol.is_none() {
-        return Err("Game board must be a square of max size 9");
+    if game_board.len() != 81 {
+        return Err("Board must be 81 symbols in length");
     }
-
-    let max_symbol = *maybe_max_symbol.unwrap();
 
     if game_board
         .iter()
-        .any(|maybe_num| maybe_num.unwrap_or(0) > max_symbol)
+        .any(|maybe_num| maybe_num.unwrap_or(0) > 9)
     {
-        return Err("Board contains a symbol that is too large");
+        return Err("Board may only contain symbols 1 to 9 or blank squares");
     }
 
-    Ok((game_board, max_symbol))
+    Ok(game_board)
 }
 
 #[cfg(test)]
@@ -41,69 +26,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_empty_single_square() {
-        assert_eq!(Ok((vec![None], 1)), parse(" "));
-    }
-
-    #[test]
-    fn parse_single_square() {
-        assert_eq!(Ok((vec![Some(1)], 1)), parse("1"));
-    }
-
-    #[test]
-    fn parse_empty_three_square() {
+    fn parse_board_too_small() {
         assert_eq!(
-            Ok((
-                vec![None, None, None, None, None, None, None, None, None],
-                3
-            )),
-            parse(",,,,,,,,")
-        );
-    }
-
-    #[test]
-    fn parse_three_square() {
-        assert_eq!(
-            Ok((
-                vec![
-                    Some(1),
-                    None,
-                    Some(2),
-                    None,
-                    None,
-                    None,
-                    Some(3),
-                    None,
-                    None
-                ],
-                3
-            )),
-            parse("1,,2,,,,3,,")
-        );
-    }
-
-    #[test]
-    fn parse_board_wrong_size() {
-        assert_eq!(
-            Err("Game board must be a square of max size 9"),
-            parse(",,,,")
+            Err("Board must be 81 symbols in length"),
+            parse(&",".repeat(79))
         );
     }
 
     #[test]
     fn parse_board_too_large() {
         assert_eq!(
-            Err("Game board must be a square of max size 9"),
-            // Board is square, but is 100 blocks - too large
-            parse(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,")
+            Err("Board must be 81 symbols in length"),
+            parse(&",".repeat(81))
         );
     }
 
     #[test]
-    fn parse_board_symbol_too_large() {
+    fn parse_board_invalid_symbol() {
         assert_eq!(
-            Err("Board contains a symbol that is too large"),
-            parse("1,2,3,4,,,,,")
+            Err("Board may only contain symbols 1 to 9 or blank squares"),
+            parse(&(",".repeat(80) + "10"))
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,16 @@ mod input_parser;
 
 fn main() {
     let input = env::args().nth(1).expect("Script accepts one argument");
-    let (game_board, max_symbol) = input_parser::parse(&input).unwrap();
-    println!("{:?}", game_board);
-    println!("{}", max_symbol);
+    let game_board = input_parser::parse(&input).unwrap();
+
+    let board_symbols: Vec<usize> = game_board
+        .iter()
+        .map(|maybe_num| maybe_num.unwrap_or(0))
+        .collect();
+
+    let board_lines = board_symbols.chunks(9);
+
+    for line in board_lines {
+        println!("{:?}", line);
+    }
 }
